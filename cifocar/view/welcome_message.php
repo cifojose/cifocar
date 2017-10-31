5ca800b7680233d199665769070165a9690e2329
jose <correojose1963@gmail.com>
2017-10-31 12:49:13 +0100

    estilo

--- cifocar/view/welcome_message.php
+++ cifocar/view/welcome_message.php
@@ -11,32 +11,34 @@
 	
 	<body>
 		<?php 
-			Template::header(); //pone el header
-
+			
 			if(!$usuario) Template::login(); //pone el formulario de login
 			else Template::logout($usuario); //pone el formulario de logout
-			
-			Template::menu($usuario); //pone el menú
 		?>
 
-		<section id="content">
-			<h2>Presentación</h2>
-			<p>Este framework para PHP ha sido desarrollado con fines docentes para el CP de 
-			<b>desarrollo de aplicaciones con tecnologías web</b> (IFCD0210) que imparte <b>Robert Sallent</b>.</p>
-			
-			<p>Es un ejemplo de <b>arquitectura modelo-vista-controlador con controlador frontal</b> para entender los
-			conceptos y poder trabajar. Además combina una parte de administración sencilla mediante interfaz gráfica, 
-			como si se tratara de un CMS, para lo que requiere de una base de datos.</p>
-			
-			<p>A lo largo del curso se desarrollan varios proyectos de ejemplo usando este pequeño framework,
-			para ir entendiendo los conceptos básicos comunes a este tipo de herramientas de trabajo. 
-			En el mismo curso, en el último módulo, utilizamos también CodeIgniter para desarrollos más complejos
-			usando la misma arquitectura.</p>
-			
-			<p><b>NO ES 100% SEGURO</b>, así que no se debe usar para desarrollos en entornos de producción. Para cualquier duda
-			o consulta,	contactad conmigo mediante twitter.</p>
-		</section>
+		<div class="centrar">
+	<figure>
+    	<img src="images/logo.png" />
+    </figure>
+
+
+  <h1>Area de login</h1>
+  
+  <form class="form" method="post" autocomplete="off">
+  	<p class="name-help">Introduzca su nombre de usuario.</p>
+    <input type="text" class="usuario" placeholder="usuario" required="required">
+    <div>
+      
+    </div>
+    <p class="email-help">Introduzca su clave de acceso.</p>
+    <input type="password" class="contraseña" placeholder="clave" required="required">
+     <div>
+      
+    </div>
+    <input type="submit" class="botonPersonalizado" value="Acceder">
+  </form>
+</div>
 		
-		<?php Template::footer();?>
+		
     </body> 
 </html>
